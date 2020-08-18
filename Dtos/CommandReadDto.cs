727ba2db7d08c0fc7791905b01fc36550a36c2ac
maestro-007 <alinajjar.91@hotmail.com>
2020-08-18 13:05:44 +0300

    Commander Project Setup

---
+++ Dtos/CommandReadDto.cs
@@ -0,0 +1,12 @@
+namespace Commander.Dtos
+{
+    public class CommandReadDto
+    {
+        public int ID { get; set; }
+        
+        public string HowTo { get; set; }
+        
+        public string Line { get; set; }
+        
+    }
+}
